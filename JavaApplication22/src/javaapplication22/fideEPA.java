3d287570decee5790ecdd9b8c392cfdd71b99a0a
KendalCM <kendalcalderon2@gmail.com>
2023-03-19 14:13:37 -0600

    Borrar comentarios

--- JavaApplication22/src/javaapplication22/fideEPA.java
+++ JavaApplication22/src/javaapplication22/fideEPA.java
@@ -1,16 +1,9 @@
 package javaapplication22;
 
-/**
- *
- * @author Usuario
- */
 public class fideEPA {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
+        System.out.println("HOLA");
     }
     
 }
